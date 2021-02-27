b0070ef6ac35734e538aea85a33561f0c1958ba3
Isaac <devisaacalejandro@gmail.com>
2021-02-27 01:51:48 -0500

    Began coding the USER class and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    binding.pry
+    "binding.pry"
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,43 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+
+  get "/signup" do
+    erb :"/users/signup.html"
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  post "/signup" do
+    user = User.new(params)
+    form_data = params.values
+    empty = ""
+    if params.has_value?(empty)
+      redirect "/signup"
+    else
+      "NO"
+    end
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
-  end
+    # # GET: /users/new
+    # get "/users/new" do
+    #   erb :"/users/new.html"
+    # end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
+  # # GET: /users/5
+  # get "/users/:id" do
+  #   erb :"/users/show.html"
+  # # end
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
+  # # GET: /users/5/edit
+  # get "/users/:id/edit" do
+  #   erb :"/users/edit.html"
+  # end
 
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+  # # PATCH: /users/5
+  # patch "/users/:id" do
+  #   redirect "/users/:id"
+  # end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+  # # DELETE: /users/5/delete
+  # delete "/users/:id/delete" do
+  #   redirect "/users"
+  # end
 end
